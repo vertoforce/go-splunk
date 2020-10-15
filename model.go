84523a50688f53ee05bd2894a8915b29471d6ac1
Andrea Vasapollo <a.vasapollo@faceit.com>
2020-10-15 15:26:08 +0100

    feat(search): added update search concurrency settings schedule

--- model.go
+++ model.go
@@ -10,3 +10,18 @@ type Paging struct {
 	PerPage int64 `json:"perPage"`
 	Offset  int64 `json:"offset"`
 }
+
+type UpdateSearchConcurrencySettingsScheduleReq struct {
+	MaxSearchesPer *int
+	AutoSummaryPer *int
+}
+
+func (u *UpdateSearchConcurrencySettingsScheduleReq) SetMaxSearchesPer(value int) *UpdateSearchConcurrencySettingsScheduleReq {
+	u.MaxSearchesPer = &value
+	return u
+}
+
+func (u *UpdateSearchConcurrencySettingsScheduleReq) SetAutoSummaryPer(value int) *UpdateSearchConcurrencySettingsScheduleReq {
+	u.AutoSummaryPer = &value
+	return u
+}
